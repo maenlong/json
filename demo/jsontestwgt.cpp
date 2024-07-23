6e8dbc62808e48509483a2acca474dcbd221f103
MEL <15801230835@163.com>
2024-07-23 10:19:34 +0800

    [code review]增加json解析异常处理，防止解析失败出现崩溃

--- demo/jsontestwgt.cpp
+++ demo/jsontestwgt.cpp
@@ -39,28 +39,39 @@ void JsonTestWgt::on_generateBtn_clicked()
 void JsonTestWgt::on_parseBtn_clicked()
 {
     std::string strJson = ui->jsonTextEdit->toPlainText().toStdString();
-    nlohmann::json json = nlohmann::json::parse(strJson);
 
-    ui->stringTextEdit->clear();
+    try
+    {
+        nlohmann::json json = nlohmann::json::parse(strJson);
 
-    if (json.contains("os") && json["os"].is_string()) {
-        std::string os = json["os"];
-        ui->stringTextEdit->append(QString("Find Key = %1 Type = %2 Value = %3").arg("os").arg("string").arg(QString::fromStdString(os)));
+        ui->stringTextEdit->clear();
+
+        if (json.contains("os") && json["os"].is_string()) {
+            std::string os = json["os"];
+            ui->stringTextEdit->append(QString("Find Key = %1 Type = %2 Value = %3").arg("os").arg("string").arg(QString::fromStdString(os)));
+        }
+
+        if (json.contains("os_version") && json["os_version"].is_string()) {
+            std::string os_version = json["os_version"];
+            ui->stringTextEdit->append(QString("Find Key = %1 Type = %2 Value = %3").arg("os_version").arg("string").arg(QString::fromStdString(os_version)));
+        }
+
+        if (json.contains("client") && json["client"].is_string()) {
+            std::string client = json["client"];
+            ui->stringTextEdit->append(QString("Find Key = %1 Type = %2 Value = %3").arg("client").arg("string").arg(QString::fromStdString(client)));
+        }
+
+        if (json.contains("client_version") && json["client_version"].is_string()) {
+            std::string client_version = json["client_version"];
+            ui->stringTextEdit->append(QString("Find Key = %1 Type = %2 Value = %3").arg("client_version").arg("string").arg(QString::fromStdString(client_version)));
+        }
     }
-
-    if (json.contains("os_version") && json["os_version"].is_string()) {
-        std::string os_version = json["os_version"];
-        ui->stringTextEdit->append(QString("Find Key = %1 Type = %2 Value = %3").arg("os_version").arg("string").arg(QString::fromStdString(os_version)));
-    }
-
-    if (json.contains("client") && json["client"].is_string()) {
-        std::string client = json["client"];
-        ui->stringTextEdit->append(QString("Find Key = %1 Type = %2 Value = %3").arg("client").arg("string").arg(QString::fromStdString(client)));
-    }
-
-    if (json.contains("client_version") && json["client_version"].is_string()) {
-        std::string client_version = json["client_version"];
-        ui->stringTextEdit->append(QString("Find Key = %1 Type = %2 Value = %3").arg("client_version").arg("string").arg(QString::fromStdString(client_version)));
+    catch (nlohmann::json::parse_error& e)
+    {
+        qDebug() << "\n nlohmann::json::parse_failed: "
+                        << "\n source = " << QString::fromStdString(strJson)
+                        << "\n reason = "<< e.what() << "\n ";
+        return;
     }
 }
 
